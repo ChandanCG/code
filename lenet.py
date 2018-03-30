1e7cff23f39c5a4bdbd6339a66cf6e3a67fdd61d
ChandanCG <chandan.cg12@gmail.com>
2018-03-30 14:15:22 +0530

    Update lenet.py

--- lenet.py
+++ lenet.py
@@ -19,13 +19,13 @@ class LeNet:
 			inputShape = (depth, height, width)
 
 		# first set of CONV => RELU => POOL layers
-		model.add(Conv2D(20, (5, 5), padding="same",
+		model.add(Conv2D(32, (3, 3), padding="same",
 			input_shape=inputShape))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
 		# second set of CONV => RELU => POOL layers
-		model.add(Conv2D(50, (5, 5), padding="same"))
+		model.add(Conv2D(64, (2, 2), padding="same"))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
@@ -33,7 +33,7 @@ class LeNet:
 		model.add(Flatten())
 		model.add(Dense(500))
 		model.add(Activation("relu"))
-
+                model.add(Dropout(0.5))
 		# softmax classifier
 		model.add(Dense(classes))
 		model.add(Activation("softmax"))

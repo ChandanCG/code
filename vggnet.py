16a738140dafa7e18b1315810ff444c5d40c2977
ChandanCG <chandan.cg12@gmail.com>
2018-04-08 16:07:14 +0530

    Update vggnet.py

--- vggnet.py
+++ vggnet.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 import cv2, numpy as np
 
